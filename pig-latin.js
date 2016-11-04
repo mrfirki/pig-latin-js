bd2c74ec306c4b601ca071cc87b1cdc5e904fc0e
gat41 <gloria.tengaihui@gmail.com>
2016-11-04 20:59:07 +0800

    first commit

---
+++ pig-latin.js
@@ -0,0 +1,13 @@
+function getPigLatin() {
+
+  //get the word input by the user
+  var str = document.getElementById("word").value;
+  
+
+  //your code goes here
+
+} //end of function getPigLatin()
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('event-btn').addEventListener("click", getPigLatin);
+});
